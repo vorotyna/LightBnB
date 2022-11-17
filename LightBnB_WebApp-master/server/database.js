7bcdfe9eb72c9a5a30a88f2bc510daf0c59bb97b
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-11-17 20:34:26 +0000

    Refactor more functions to retrieve date from database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,48 +1,80 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
 
-/// Users
+// DATABASE CONNECTION
+const { Pool } = require('pg');
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
 
-/**
- * Get a single user from the database given their email.
- * @param {String} email The email of the user.
- * @return {Promise<{}>} A promise to the user.
- */
+
+// --- USERS --- // 
+
+// Get a single user from the database given their email
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  const queryString = `
+  SELECT * 
+  FROM users
+  WHERE users.email = $1
+  `;
+
+  return pool.query(queryString, [email])
+    .then(response => {
+      if (response.rows) {
+        return response.rows[0];
+      } else {
+        return null;
+      }
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
 };
+
 exports.getUserWithEmail = getUserWithEmail;
 
-/**
- * Get a single user from the database given their id.
- * @param {string} id The id of the user.
- * @return {Promise<{}>} A promise to the user.
- */
+// Get a single user from the database given their ID
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  const queryString = `
+  SELECT * 
+  FROM users
+  WHERE users.id = $1
+  `;
+
+  return pool.query(queryString, [id])
+    .then(response => {
+      if (response.rows) {
+        return response.rows[0];
+      } else {
+        return null;
+      }
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
 };
+
 exports.getUserWithId = getUserWithId;
 
 
-/**
- * Add a new user to the database.
- * @param {{name: string, password: string, email: string}} user
- * @return {Promise<{}>} A promise to the user.
- */
+// Add a new user to the database.
 const addUser = function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  const queryString = `
+  INSERT INTO users (name, email, password)
+  VALUES ($1, $2, $3)
+  RETURNING *;
+  `;
+
+  return pool.query(queryString, [user.name, user.email, user.password])
+    .then(response => {
+      return response.rows[0];
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
 };
 exports.addUser = addUser;
 
@@ -66,18 +98,6 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-
-
-// DATABASE CONNECTION
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  user: 'vagrant',
-  password: '123',
-  host: 'localhost',
-  database: 'lightbnb'
-});
-
 
 
 // QUERYING THE DATABASE
